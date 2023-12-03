489520b26430d0f969ee8444b0c27e800ece04ea
Kaj Jansson <kaj.jansson@myy.haaga-helia.fi>
2023-12-03 19:47:52 +0200

    Deploying personal trainer, part 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/jansson_Personal_Trainer/',
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
